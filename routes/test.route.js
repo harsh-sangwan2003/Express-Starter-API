de1fe901ba1540dff00beeb9545e0df355950006
harsh-sangwan2003 <hsangwan2002@gmail.com>
2023-12-24 18:30:30 +0530

    added

--- routes/test.route.js
+++ routes/test.route.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-router.get("/test",(req,res)=>{
+router.get("/test", (req, res) => {
 
-    res.status(200).json({message:"Test successful"});
+    res.status(200).json({ message: "Test successful" });
 })
 
 module.exports = router;
