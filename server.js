8e74fffac7d51901897f6f9fc5cee226a5db3681
harsh-sangwan2003 <hsangwan2002@gmail.com>
2023-12-24 18:26:44 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const testRoute = require('./routes/test.route');
+
+const app = express();
+app.use(express.json());
+
+app.use("/api",testRoute);
+
+app.listen(5000,()=>{
+    console.log("Backend server is running.");
+})
